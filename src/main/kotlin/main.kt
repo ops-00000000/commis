adf02db59a7d92b39fd0087dfed27586c8549ac5
Vladimir <belaevvova999@gmail.com>
2020-12-27 18:59:36 +0300

    Second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,40 @@
 fun main(){
-    fun commis(){
-        val amount: Double = 40000.0
-        var coef: Double
-        var comm: Double
-        val card: String = "Visa"
-        when(card) {
-            "Visa","МИР" -> {
-                coef = 0.075
-                comm = amount * coef
-                if (comm <= 3500) {
-                    comm = 3500.0
-                }
-            }
-            "Mastercard","Maestro" -> {
-                if(amount >= 30000 && amount <= 7500000 ){
-                    comm = 0.0
-                }
-                else{
-                    coef = 0.06
-                    comm = amount * coef + 20
-                }
-            }
-        else -> comm = 0.0
-        }
-        println("Ваша комиссия: $comm")
- }
     commis()
+}
+
+fun commis(){
+    val amount: Double = 40000.0
+    var coef: Double
+    val comm: Double
+    val card: String = "Visa"
+    when(card) {
+        "Visa","МИР" -> comm = visaWorld(amount)
+        "Mastercard","Maestro" -> comm = mm(amount)
+        else -> comm = 0.0
+    }
+    println("Ваша комиссия: $comm")
+}
+
+fun visaWorld(amount: Double):Double{
+    val coef: Double
+    var comm: Double
+    coef = 0.075
+    comm = amount * coef
+    if (comm <= 3500) {
+        comm = 3500.0
+    }
+    return comm
+}
+
+fun mm(amount: Double):Double{
+    val coef: Double
+    val comm: Double
+    if(amount >= 30000 && amount <= 7500000 ){
+        comm = 0.0
+    }
+    else{
+        coef = 0.06
+        comm = amount * coef + 20
+    }
+    return  comm
 }
